0e1873e015bbdc3154db62aac6271f56413c5347
Azats174 <55838320+Azats174@users.noreply.github.com>
2019-09-26 16:50:40 +0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if ! [ -d test ]; then
+mkdir test
+fi
+mktemp  test/test.XXXXX
+logger -s "greate random file"
+find test -type f   -cmin +1  -delete -exec logger -s "remove  old files" \;
